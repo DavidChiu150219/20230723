808ecf1d3afebccfc546b1433a2cfe36ff095248
DavidChiu150219 <140772034+DavidChiu150219@users.noreply.github.com>
2023-08-13 03:58:49 +0000

    modify

---
+++ 巨匠.py
@@ -0,0 +1,32 @@
+while True:
+    import random 
+    mini=int(input('Please enter the minimum:'))
+    maxi=int(input('Please enter the maximum:'))
+    n=random.randint(mini,maxi)
+    t=0
+
+    while True:
+        x=int(input(f'Please enter a number between {mini} and {maxi}:'))
+        t+=1
+        if x<mini or x>maxi:
+            print('out of range')
+        else:
+            if x!=n:
+                if x>n:
+                    print('The answer is smaller')
+                    maxi=x
+                else:
+                    print('The answer is bigger')
+                    mini=x
+            else:
+                break
+    print(f'Congratulations! The answer is {n}')
+
+    try:
+        a=str(input('Do you want to continue?(y or n)'))
+    except:
+        print('Enter again!')
+    else:
+        if a=='n':
+            break
+print('This is the end of the game')
